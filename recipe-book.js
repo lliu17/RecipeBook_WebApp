8aaeac1c2e2db02010e5e2cc3380658c3af046b2
lliu7 <lliu7@artic.edu>
2020-12-11 07:32:41 -0600

    adding directly to db (checking done in form)

--- recipe-book.js
+++ recipe-book.js
@@ -4,40 +4,34 @@ const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb+srv://grandma:grandma123@cluster0.hdzif.mongodb.net/?retryWrites=true&w=majority";
 
-// display thank you message after post action
+// display thank you message after submitting recipe (post action)
 router.get("/", (req, res) => { 
-    res.send("Thank you for submitting a recipe!");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("Thank you for submitting a recipe!<br>");
+    res.write('<a href="/recipe-book.html">Add another recipe</a>');
+    res.end();
 });
 
 // add new -> post request
 router.post("/", (req, res) => {
-    console.log("here");
-    const newRecipe = {
-        name: req.body.recipeName
-    }
+    // note: valid input not checked here! should be checked in the form before submission
+    const newRecipe = req.body;
+    console.log(newRecipe);
 
-    if (!newRecipe.name) {  // bad request
-        res.status(400).json({msg:"Please include a name"});    
-    }
-    else {
-        console.log(newRecipe);
+    MongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) => {
+        if (err) return console.error(err);
+        else console.log("connected to db");
 
-        MongoClient.connect(url,  { useUnifiedTopology: true }, (err, client) => {
+        const db = client.db("FamilyRecipe");
+        const collection = db.collection("recipes");
+        collection.insertOne(newRecipe, (err, res) => 
+        {
             if (err) return console.error(err);
-            else console.log("connected to db");
-    
-            const db = client.db("FamilyRecipe");
-            const collection = db.collection("recipes");
-            collection.insertOne(newRecipe, (err, res) => 
-            {
-                if (err) return console.error(err);
-                else console.log(`data inserted: ${res.insertedCount} rows`);
-                client.close();
-            });
+            else console.log(`data inserted: ${res.insertedCount} rows`);
+            client.close();
         });
-
-        res.redirect("/recipes");
-    }
+    });
+    res.redirect("/recipes");
 });
 
 module.exports = router;
