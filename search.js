352a1fbf7ec13dfaafcfa070c8fecacfefb115c6
Elizabeth Hom <hom.elizabeth@yahoo.com>
2020-12-14 14:02:03 -0500

    Adding ingredients

--- search.js
+++ search.js
@@ -54,6 +54,7 @@ function printItems(result, res) {
     var recipesToPrint = "<head><link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'><link rel='stylesheet' href='recipe-book.css'></head><body><div class='search-results'><h1 style='text-align: center;'>Search Results</h1><ul class='nav-content' style='background-color: rgba(255, 255, 150, .5);'><li class='nav-li'><a href='index.html'>HOME</a></li>&nbsp;&nbsp;&bull;&nbsp;&nbsp;<li class='nav-li'><a href='recipe-book.html'>CREATE</a></li>&nbsp;&nbsp;&bull;&nbsp;&nbsp;<li class='nav-li'><a href='search.html'>SEARCH</a></li>&nbsp;&nbsp;&bull;&nbsp;&nbsp;<li class='nav-li'><a href='about.html'>ABOUT US</a></li>&nbsp;&nbsp;&bull;&nbsp;<li class='nav-li'><a href='contact.html'>CONTACT</a></li></ul><div id='resultsDiv'>";
     var numRecipes = 1;
     var numUtensils = 1;
+    var numIngred = 1;
 
     if (result.length > 0) {
         for (var i = 0; i < result.length; i++) {
@@ -61,10 +62,10 @@ function printItems(result, res) {
             recipesToPrint += "<p class='recipeContent'>Time: " + result[i].hours + " hours, " + result[i].minutes + " minutes</p>";
             recipesToPrint += "<p class='recipeContent'>Utensils:</p>";
             for (var j = 0; j < result[i].utensils.length; j++) {
-                recipesToPrint += "<p class='recipeContent'>" + numUtensils + ". " +  result[i].utensils[j] + "</p>";
+                recipesToPrint += "<p class='recipeContent'>" + numUtensils + ". &nbsp;&nbsp;&nbsp;" + result[i].utensils[j] + "</p>";
                 numUtensils++;
             }
-
+            recipesToPrint += "<p class='recipeContent'>Ingredients:</p>";
 
             numUtensils = 0;
             numRecipes++;
